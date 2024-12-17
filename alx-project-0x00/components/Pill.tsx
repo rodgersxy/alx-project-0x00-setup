c36ecd824459c0e4617a47e10d9431b2ede0d828
rodgersxy <rodgersny99@gmail.com>
2024-12-17 19:06:12 +0300

    add: Typing your functional components

--- alx-project-0x00/components/Pill.tsx
+++ alx-project-0x00/components/Pill.tsx
@@ -1,13 +1,11 @@
-interface PillProps {
-  title: string; // Define title as a prop
-}
+import { PillProps } from "@/interfaces";
 
 const Pill: React.FC<PillProps> = ({ title }) => {
-  return (
-    <div className="flex justify-center items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
-      <p className="text-sm">{title}</p>
-    </div>
-  );
-};
-
-export default Pill;
+    return (
+      <div className=" flex justify-center  items-center bg-[#F9F9F9] px-3 w-auto h-[27px] rounded-full">
+        <p className=" text-sm ">{title}</p>
+      </div>
+    )
+  }
+  
+  export default Pill;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -5,6 +5,7 @@ const Landing: React.FC = () => {
       <div>
         <h1 className="text-xl font-extralight">Landing Page</h1>
         <Card />
+        
       </div>
     );
   };
